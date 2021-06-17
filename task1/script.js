f06003cc426f1df1c86a16f7bbda708b250d57c2
fellainthewagon <fellainthewagon@gmail.com>
2021-06-18 02:39:10 +0300

    part_2 done dropdown menu

--- task1/script.js
+++ task1/script.js
@@ -1,7 +1,17 @@
-const menu = document.querySelector(".menu");
+const container = document.querySelector(".container");
+const dropDown = document.querySelector(".drop");
+const list = document.querySelector(".list");
 
-menu.addEventListener("click", openMenu);
+container.addEventListener("click", (e) => {
+  if (
+    dropDown.classList.contains("active") &&
+    e.target.closest(".list") === list
+  )
+    return;
 
-function openMenu() {
-  document.querySelector(".drop").classList.toggle("active");
-}
+  if (e.target.className === "menu") {
+    dropDown.classList.toggle("active");
+  } else {
+    dropDown.classList.remove("active");
+  }
+});
